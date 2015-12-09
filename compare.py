151178fa035e07ea845373b6f071bc6a5c3dbcdd
chenyuntc <chenyuntc@163.com>
2015-12-09 20:50:49 +0800

    新增从mongodb 中获取墨迹天气数据, 与我们的数据进行对比

---
+++ compare.py
@@ -0,0 +1,65 @@
+#!coding:utf8
+__author__ = 'cy'
+import pymongo
+import urllib2
+from urllib2 import urlopen
+
+
+
+### 读取area_coor.txt 获取地名和id的对应关系
+f = open('area_coor.txt')
+remind_info=[]
+all_lines = f.readlines()
+f.close()
+result = map(lambda x: (x.split(' ')[2], x.split(' ')[3]), all_lines)
+d = {}
+for ii in result:
+    d[ii[0].decode('utf8')] = ii[1]
+
+
+# mongodb中每次采集墨迹的数据久新建一个collection, collection的名字是取值当时的时间,
+#  所以对collection的名字进行排序之后, 最靠后的是一个系统的collection, 第二靠后的一个就是最新采集的墨迹空气质量信息
+client=pymongo.MongoClient('54.223.178.198',27110)#client=pymongo.MongoClient('172.31.11.244',27110)
+db=client.test
+collections=db.collection_names()
+collections.sort()
+collection=db[collections[-2]]
+
+### 获取mongodb(即墨迹)中的城市空气质量信息
+# 墨迹天气中地名用的是`海淀区 `,`郑州市`,`塔里木地区`,而我们的地名是`海淀`,`郑州`,`塔里木`
+# 墨迹总共采集了2500个左右的城市, 其中能在我们的数据中找到对应的有2100多个
+all_cities=[]
+for ii in collection.find():
+    begin_time=ii[u'city']['time'].replace(' ','-').replace(':','-').split('-')[:4]
+    b=ii[u'city']['cityname']
+    if d.has_key(b[:-1]):
+        id=d[b[:-1]]
+    elif d.has_key(b[:-2]) and len(b[:-2])>=2:id=d[b[:-2]]
+    else :continue
+    try:
+        all_cities.append({'time':begin_time,'info':ii[u'city']['list'][0]['list'],'id':id})
+    except:
+        print ii
+
+# 获取我们的空气数据 测试只取了300个
+for ii in all_cities[:300]:
+    begin_time=''
+    for tt in ii['time']:begin_time=begin_time+tt
+    end_time=ii['time'][0]+str(ii['time'][1]+str(int(ii['time'][2])+1))+ii['time'][3]
+    url='http://openapi.mlogcn.com:8000/api/aqi/fc/area/'+ii['id']+'/h/'+ begin_time+'/'+end_time +u'.json\
+?appid=27fbe0976bd14ec397cd37add0526bf2&timestamp=1442477082312&key=mqC93zaI9x3whSEEasRHfOMO8bI%3D'
+
+    r=urlopen(url)
+    rr=eval(r.read())
+    if 'errorCode'  in rr:
+        print '--------------------'
+        print 'error'
+        print ii,url
+        print '--------------------'
+    mlog_data=rr['series']
+    moji_data=[qq['aqi'] for qq in ii['info']]
+    for ii in zip(moji_data,mlog_data):
+        # 如果差距过大, 就提醒
+        if float(abs(ii[0]-ii[1]))/(ii[0]+ii[1])>0.2 and    abs(ii[0]-ii[1])>50 :
+            remind_info.append({'mlog_data':mlog_data,'moji_data':moji_data})
+
