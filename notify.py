ccb44ba3b3f5cfd87bbb8e2516b9cf7fa85640ad
chenyuntc <chenyuntc@163.com>
2015-12-10 17:40:15 +0800

    发送消息大致框架

--- notify.py
+++ notify.py
@@ -1,1 +1,29 @@
 __author__ = 'cy'
+from hypchat import HypChat
+
+__users__ = ['cyfall@163.com', 'b@b.com']
+__token__='autC8mgWDMxXdndDirfi7YpSeNygh96b96FXwIZY'
+def wrap(message):
+    return message
+
+
+def send_notify(data):
+
+    hp = HypChat(__token__)
+    for user in __users__:
+        try:
+            t_user = hp.get_user(user)
+            message=wrap(data)
+            t_user.message(message)
+        except Exception as e:
+            print e
+    return True
+
+
+
+send_notify('hello there')
+
+
+
+
+
