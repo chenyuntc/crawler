f22d799a6935321e461f66e012897f44cfc302f9
chenyuntc <chenyuntc@163.com>
2015-12-15 11:31:05 +0800

    新增处理晚上八点预测第二天数据的mae和mse的计算,以及基于等级的mae和mse的计算

--- processfile.py
+++ processfile.py
@@ -1,52 +1,17 @@
 # coding:utf8
+import copy
 from math import sqrt
 import os
 import time
 import pymongo
 host='54.223.178.198'
+from util import  get_days,get_files,connect
 # files = ['pm2_5201512100' + str(ii) for ii in range(1, 10)]
 # all_data = []
 start_time = '15-11-01'
-end_time = '15-11-19'
+end_time = '15-11-02'
 gaps = range(1, 25)
 path='/home/cy/tmp/pm/'
-
-def get_files(start_time, end_time):
-    '''
-    :param start_time: 起始时间 格式如'15-12-01'
-    :param end_time: 终止时间 格式同上
-    :return: 所有需要处理的文件 ['pm2_5201512100','pm2_5201512101','pm2_5201512102'.....]
-    '''
-    s_ = start_time.split('-')
-    start = int(time.strftime('%j', time.strptime(start_time, '%y-%m-%d')))
-    end = int(time.strftime('%j', time.strptime(end_time, '%y-%m-%d')))
-    all_files = []
-    for ii in range(start, end + 1):
-        tmp_time = time.strptime('15-%s' % ii, '%y-%j')
-        tmp_day = time.strftime('%Y%m/%d/', tmp_time)
-        tmp_day2 = time.strftime('pm2_5%Y%m%d', tmp_time)
-        h_24 = ['0' + str(ii) for ii in range(10)] + [str(ii) for ii in range(10, 24)]
-        b = [tmp_day + tmp_day2 + (ii) for ii in h_24]
-        all_files += b
-    return all_files;
-
-def get_days(start_time, end_time):
-    '''
-    :param start_time: 起始时间 格式如'15-12-01'
-    :param end_time: 终止时间 格式同上
-    :return: 所有需要处理的文件 ['pm2_5201512100','pm2_5201512101','pm2_5201512102'.....]
-    '''
-    s_ = start_time.split('-')
-    start = int(time.strftime('%j', time.strptime(start_time, '%y-%m-%d')))
-    end = int(time.strftime('%j', time.strptime(end_time, '%y-%m-%d')))
-    all_files = []
-    for ii in range(start, end + 1):
-        tmp_time = time.strptime('15-%s' % ii, '%y-%j')
-        tmp_day = time.strftime('%Y%m/%d/', tmp_time)
-        tmp_day2 = time.strftime('pm2_5%Y%m%d', tmp_time)
-        b = [tmp_day + tmp_day2 ]
-        all_files += b
-    return all_files;
 
 def map_line(x):
     '''从文件的每一行读取有用信息'''
@@ -61,15 +26,13 @@ def map_line(x):
 def map_24h(data):
 
     q=data[20::24]
+    q=copy.deepcopy(q)
     for time1 in q:
         if time1==-1:continue
         for city in time1:
             time1[city]=time1[city][0::24]
     return q
-def connect(host):
-    client = pymongo.MongoClient(host, 27110)
-    db = client.test
-    return db
+
 def check(all_lines):
     '''
     简单的检查, 并不保证文件百分百正确, 但是出错概率极低
@@ -100,6 +63,61 @@ def calculate(all_data, gaps=[1], city=0):
         results[1].append(sqrt(float(mse) / real_len))
     return results
 
+def calculate2(x,y):
+    f=filter(lambda x:(x[0]+1)*(x[1]+1)!=0,zip(x,y))
+    mse=map(lambda  x:(x[0]-x[1])**2,f)
+    mae=map(lambda  x:abs(x[0]-x[1]),f)
+    c=len(f)
+    mae=reduce(lambda a,b:a+b,mae)
+    mse=reduce(lambda a,b:a+b,mse)
+    return [float(mae)/c,sqrt(float(mse)/c)]
+
+def get_sample(all_data):
+    '''
+    获取每晚八点预测的第二天的天气, 获取第二天24小时的实测数值
+    :param all_data:
+    :return:[ 第二天实测值,8点预测数值]
+    '''
+    #每天实测值
+    t1={}
+    for city in city_ids:
+        t1[city]=list()
+
+    for time1 in all_data[24:]:
+        if time1==-1:
+            for nouse in t1:
+                t1[nouse].append(-1)
+        else:
+            for ii in time1:
+                if t1.has_key(ii):
+                    t1[ii].append(time1[ii][0])
+    # 八点预测值
+    t2={}
+    for city in city_ids:
+        t2[city]=list()
+    for time1 in all_data[20:-24:24]:
+            if time1==-1:
+                for ii in city_ids:
+                        t2[ii]+=[-1 for i in range(24)]
+            else:
+                for city in time1:
+                    if t1.has_key(city):
+                        t2[city]+=time1[city][4:28]
+    return (t1,t2)
+
+
+def map2(data):
+    r=[];
+    for time in data:
+        if time==-1:
+            r.append(-1)
+            continue
+        tmp={}
+        for city in time:
+
+            tmp[city]=map(lambda x :x/5+1,time[city])
+        r.append(tmp)
+    return r
 def write2db(db,results,start_time,end_time,city_id):
     c = db.compare_results
     c.insert_one({
@@ -140,34 +158,20 @@ if __name__ == '__main__':
     c = db.compare_results
     c.delete_many({ 'key':'compare_results'})
     city_ids=db.compare_results.find_one({'key':'station_id'})['data']
+    t_24=get_sample(all_data)
+    nouse={}
+    level_datas=map2(all_data)
     for ii in city_ids:
         results = calculate(all_data, gaps, ii)
-        t_20=map_24h(all_data)
-        results_24=calculate(t_20,[1],ii)
+        results_24=[0,0]
+        results_24=calculate2(t_24[0][ii],t_24[1][ii])
         results+=results_24
-        write2db(db,results, start_time, end_time,ii)
+        result_level=calculate(level_datas,gaps,ii)
+        results+=result_level
+        nouse[ii]=results
+        #write2db(db,results, start_time, end_time,ii)
 
 
-def calculate2(all_data, gaps=[1], city=0):
-    '''
-   :param all_data: 所有的数据, 每一行代表着一个时间的数据,
-   :param gaps: 所有要处理的间隔
-   :param city: 监测站编号
-   :return: 所有间隔(矩阵 2*len(gaps))
-     '''
-    all_data=all_data[20::24]
-    results = [list(), list()]
-    for gap in gaps:
-        real_len = 0
-        mae = mse = 0
-        for ii in range(len(all_data) - gap):
-            if all_data[ii] == -1 or all_data[ii + gap] == -1:
-                continue
-            real_len += 1
-            mae += abs(all_data[ii][city][gap] - all_data[ii ][city][0])
-            mse += (all_data[ii][city][gap] - all_data[ii ][city][0]) ** 2
-        results[0].append(float(mae) / real_len)
-        results[1].append(sqrt(float(mse) / real_len))
-    return results
+
 
 
