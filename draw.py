3bb00b7fb05f2e6e52c7de7d834838965dae9875
chenyuntc <chenyuntc@163.com>
2015-12-31 18:08:21 +0800

    新增多幅图的绘制 代码未整理

--- draw.py
+++ draw.py
@@ -4,6 +4,7 @@ capital_num,day_num=31,7
 from util import  connect
 from matplotlib import  pyplot as plt
 import  numpy as np
+summary_data=np.zeros([24,10])
 
 db=connect(host='54.223.178.198', port=27110)
 
@@ -17,18 +18,19 @@ hour_num=24
 
 
 all_data2={}
-for ii in c.find({'key':'compare_results','end_time':'15-11-7'}):
+for ii in c.find({'key':'compare_results','end_time':'15-12-30'}):
     all_data2[ii['city']]=(ii)
 all_data=all_data2
 
 capital=db.capital
-cities=capital.find_one({})
+cities=capital.find_one({'key':'capital'})
 x_ticks=[ii[0] for ii in cities['data']]
 capital_data={}
 for ii in cities['data']:
     capital_data[ii[0]]=all_data2[ii[4]]
 
 
+zone=capital.find_one({'key':'zone'})
 
 
 def  draw_mae(cities,start_time=None,end_time=None):
@@ -102,12 +104,6 @@ def  draw_day_mse(cities):
     plt.show()
 
 cities=['200'+str(ii)+'A' for ii in range(10)]
-
-#draw_mae(cities)
-#draw_mse(cities)
-
-
-
 
 
 def draw_sqrt(city):
@@ -183,26 +179,30 @@ def draw_avg():
     a=np.ones([station_num, hour_num])
     jj=0
     for ii in all_data2.values():
-        a[jj]=ii['data'][1]
+        a[jj]=ii['data'][0]
         jj+=1
     a=np.array(filter(lambda  x: (x[0]+1)*x[0]!=0, a))
     avg=a.mean(axis=0)
     print avg
-    plt.plot(avg,label='MAE')
-    plt.title(u"所有站点 AQI数值MAE")
+    summary_data[:,0]=avg
+    x=range(1,25)
+    plt.plot(x,avg,label='MAE')
+    plt.title(u"所有站点 AQI数值MAE和mse")
     plt.xlabel(u"小时")
     plt.grid(True)
     a=np.ones([station_num, hour_num])
     jj=0
     for ii in all_data2.values():
-        a[jj]=ii['data'][0]
+        a[jj]=ii['data'][1]
         jj+=1
     a=a[a!=-1].reshape([-1,24])
     avg=a.mean(axis=0)
-    plt.plot(avg,label='MSE')
+    summary_data[:,1]=avg
+    plt.plot(x,avg,label='MSE')
     print avg
     xx=range(1,25)
     plt.xticks(xx)
+
 
 
     plt.title(u"所有站点 AQI数值MSE")
@@ -213,9 +213,9 @@ def draw_avg():
     plt.show()
 
 def draw_level_avg():
-    level_data=np.zeros([station_num,hour_num])
+    level_data=np.zeros([station_num,hour_num])-2
     jj=0
-    for ii in all_data:
+    for ii in all_data.values():
         kk=0
         for iii in ii['data'][3]:
            # if ii['data'][3][kk].get('0'): level_data[jj][kk]=-1;kk+=1;continue
@@ -223,10 +223,12 @@ def draw_level_avg():
             kk+=1
         jj+=1
 
-    level_data=level_data[level_data!=0].reshape([-1,24])
+    level_data=level_data[level_data!=-2].reshape([-1,24])
     avg_level=level_data.mean(axis=0)
-    plt.plot(avg_level)
+    x=range(1,25)
+    plt.plot(x,avg_level)
     print avg_level
+    summary_data[:,2]=avg_level
     plt.title(u'AQI等级差为0(%)')
     plt.xlabel(u'小时')
     xx=range(1,25)
@@ -235,8 +237,9 @@ def draw_level_avg():
     plt.show()
 
 
+
 def draw_level_avg_2():
-    level_data=np.zeros([station_num,hour_num])
+    level_data=np.zeros([station_num,hour_num])-2
     jj=0
     for ii in all_data.values():
         kk=0
@@ -245,10 +248,12 @@ def draw_level_avg_2():
             level_data[jj][kk]=ii['data'][3][kk].get('0',0)+ii['data'][3][kk].get('1',0)
             kk+=1
         jj+=1
-    level_data=level_data[level_data!=0].reshape([-1,24])
+    level_data=level_data[level_data!=-2].reshape([-1,24])
     avg_level=level_data.mean(axis=0)
-    plt.plot(avg_level)
+    x=range(1,25)
+    plt.plot(x,avg_level)
     print avg_level
+    summary_data[:,9]=avg_level
     plt.title(u'AQI等级差为0,1(%)')
     plt.xlabel(u'小时')
     plt.grid(True)
@@ -257,21 +262,10 @@ def draw_level_avg_2():
     plt.show()
 
 def draw_city_mse():
-    city_mae=np.zeros([capital_num,day_num])
-    jj=0
-    new_x_ticks=[ii[:-1] for ii in x_ticks]
-    for ii in x_ticks:
-
-        city_mae[jj]=capital_data[ii]['data'][2][0][:day_num]
-        jj+=1
-    for ii in range(day_num):
-        plt.bar(range(1,capital_num+1),city_mae[:,ii])
-        plt.xticks(np.linspace(1.5,capital_num+0.5,capital_num ),new_x_ticks)
-
-        plt.show()
-
-
-def draw_city_mae():
+    '''
+    话所有城市的mse 柱状图 总共话7张或者9张 ,每张包含31个城市的mse
+    :return:
+    '''
     city_mae=np.zeros([capital_num,day_num])
     jj=0
     new_x_ticks=[ii[:-1] for ii in x_ticks]
@@ -279,13 +273,104 @@ def draw_city_mae():
 
         city_mae[jj]=capital_data[ii]['data'][2][1][:day_num]
         jj+=1
+    for ii in range(day_num):
+        plt.bar(range(1,capital_num+1),city_mae[:,ii])
+        plt.xticks(np.linspace(1.5,capital_num+0.5,capital_num ),new_x_ticks)
+        plt.show()
+
+
+
+def draw_city_mse():
+    '''
+    话所有城市的mae 柱状图 总共话7张或者9张 ,每张包含31个城市的mae
+    :return:
+    '''
+    city_mae=np.zeros([capital_num,day_num])
+    zone_mae={}
+    jj=0
+    new_x_ticks=[ii[:-1] for ii in x_ticks]
+    for ii in x_ticks:
+
+        city_mae[jj]=capital_data[ii]['data'][2][1][:day_num]
+        jj+=1
+    # for ii in range(capital_num):
+    #     plt.plot(range(1,day_num+1),city_mae[ii,:],label=x_ticks[ii])
+    #     if (ii!=0 and  ii%5==0) or ii==capital_num-1:
+    #         plt.legend(loc='best')
+    #         plt.show()
+
+    for  every_zone in zone['data']:
+        max_num=[]
+        for every_city in zone['data'][every_zone]:
+
+            plt.plot(np.arange(1,day_num+1) ,capital_data[every_city+u'市']['data'][2][1][:day_num],'-o',\
+                     label=every_city, linewidth='2.0')
+            max_num.append(max(capital_data[every_city+u'市']['data'][2][1][:day_num] ))
+        plt.title(every_zone+u'地区7天预测MSE')
+        plt.legend(loc='best')
+        plt.xlabel(u'提前天数')
+        plt.ylim([0,max(max_num)*1.1])
+        plt.grid(True)
+        plt.savefig(u'mse_'+unicode(every_zone))
+
+        plt.show()
+def draw_city_mae():
+    '''
+    话所有城市的mae 柱状图 总共话7张或者9张 ,每张包含31个城市的mae
+    :return:
+    '''
+    city_mae=np.zeros([capital_num,day_num])
+    zone_mae={}
+    jj=0
+    new_x_ticks=[ii[:-1] for ii in x_ticks]
+    for ii in x_ticks:
+
+        city_mae[jj]=capital_data[ii]['data'][2][0][:day_num]
+        jj+=1
+    # for ii in range(capital_num):
+    #     plt.plot(range(1,day_num+1),city_mae[ii,:],label=x_ticks[ii])
+    #     if (ii!=0 and  ii%5==0) or ii==capital_num-1:
+    #         plt.legend(loc='best')
+    #         plt.show()
+
+    for  every_zone in zone['data']:
+        max_num=[]
+        for every_city in zone['data'][every_zone]:
+
+            plt.plot(np.arange(1,day_num+1) ,capital_data[every_city+u'市']['data'][2][0][:day_num],'-o',\
+                     label=every_city, linewidth='2.0')
+            max_num.append(max(capital_data[every_city+u'市']['data'][2][0][:day_num] ))
+        plt.title(every_zone+u'地区7天预测mae')
+        plt.legend(loc='best')
+        plt.xlabel(u'提前天数')
+        plt.ylim([0,max(max_num)*1.1])
+        plt.grid(True)
+        plt.savefig(u''+unicode(every_zone))
+
+        plt.show()
+
+
+def generate_csv():
+    city_mae=np.zeros([capital_num,day_num])
+    city_data={}
+    zone_mae={}
+    jj=0
+    f=open('city_data.csv','w' )
+    new_x_ticks=[ii[:-1] for ii in x_ticks]
+    for ii in x_ticks:
+
+       # city_mae[jj]=capital_data[ii]['data'][2][0][:day_num]
+        city_data[ii]=capital_data[ii]['data']
+        jj+=1
+    tmp=np.zeros([31,18])
+    jj=0
+    for ii in city_data:
+        print(ii)
+        tmp[jj]=city_data[ii][2][0]+city_data[ii][2][1]
+        jj+=1
     for ii in range(capital_num):
-        plt.plot(range(1,day_num+1),city_mae[ii,:],label=x_ticks[ii])
-        if (ii!=0 and  ii%5==0) or ii==capital_num-1:
-            plt.legend(loc='best')
-            plt.show()
-
-    #plt.xticks(np.linspace(1.5,capital_num+0.5,capital_num ),new_x_ticks)
-#  for ii in cities['data']:
-
-
+        pass
+        #plt.plot(range(1,day_num+1),city_mae[ii,:],label=x_ticks[ii])
+        #if (ii!=0 and  ii%5==0) or ii==capital_num-1:
+          #  plt.legend(loc='best')
+            #plt.show()
