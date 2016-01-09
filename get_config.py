84841b11ffca3d9ea2e03912b9039e580f216b05
chenyuntc <chenyuntc@163.com>
2016-01-09 13:31:03 +0800

    将配置文件读取操作集中到一个专门的文件中, 优化代码结构

---
+++ get_config.py
@@ -0,0 +1,23 @@
+#coding:utf8
+from ConfigParser import ConfigParser
+import pymongo
+from util import get_files
+
+__author__ = 'cy'
+cf = ConfigParser()
+cf.read('dev.cfg')
+city_num = int(cf.get('calculate', 'city_num'))
+predict_hour = int(cf.get('calculate', 'predict_hour'))
+file_path = cf.get('file', 'filepath')
+start_time = cf.get('file', 'start_time')
+end_time = cf.get('file', 'end_time')
+gaps = eval(cf.get('calculate', 'gaps'))
+all_files = (get_files(start_time, end_time))
+day_gaps=cf.get('calculate','day_gaps')
+mongo_host=cf.get('mongodb','host')
+mongo_port=int(cf.get('mongodb','port'))
+mongo_db_name=cf.get('mongodb','db_name')
+client=pymongo.MongoClient(mongo_host,mongo_port)
+db=client.get_database(mongo_db_name)
+bins = eval(cf.get('calculate', 'bins'))
+col=cf.get('mongodb','collection')
