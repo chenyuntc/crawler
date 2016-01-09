a6abe77fd18d79f9e06095438ec48b454312f496
chenyuntc <chenyuntc@163.com>
2016-01-09 15:55:53 +0800

    修改get_files 可取任意一段时间内的所有文件名 最初只能用处理同一年之内的连续多天

--- util.py
+++ util.py
@@ -63,39 +63,22 @@ def connect(host,port=27110):
 
 
 def get_files(start_time, end_time):
+    # FIXED TODO : 目前只能处理2015 年 的时间
     '''
-    todo: 目前只能处理2015 年 的时间
-    :param start_time: 起始时间 格式如'15-12-01'
+    :param start_time: 起始时间 格式如'2015-12-01'
     :param end_time: 终止时间 格式同上
     :return: 所有需要处理的文件 ['pm2_5201512100','pm2_5201512101','pm2_5201512102'.....]
     '''
-    s_ = start_time.split('-')
-    start = int(time.strftime('%j', time.strptime(start_time, '%Y-%m-%d')))
-    end = int(time.strftime('%j', time.strptime(end_time, '%Y-%m-%d')))
+    start_time=time.mktime(time.strptime(start_time,'%Y-%m-%d'))
+    end_time=time.mktime(time.strptime(end_time,'%Y-%m-%d'))
     all_files = []
-    for ii in range(start, end + 1):
-        tmp_time = time.strptime('15-%s' % ii, '%y-%j')
-        tmp_day = time.strftime('%Y%m/%d/', tmp_time)
+    for every_day in xrange(int(start_time),int(end_time),86400):
+        tmp_time=time.strptime(time.ctime(every_day))
+        tmp_day=time.strftime('%Y%m/%d/',tmp_time)
         tmp_day2 = time.strftime('pm2_5%Y%m%d', tmp_time)
         h_24 = ['0' + str(ii) for ii in range(10)] + [str(ii) for ii in range(10, 24)]
         b = [tmp_day + tmp_day2 + (ii) for ii in h_24]
         all_files += b
     return all_files;
 
-def get_days(start_time, end_time):
-    '''
-    :param start_time: 起始时间 格式如'15-12-01'
-    :param end_time: 终止时间 格式同上
-    :return: 所有需要处理的文件 ['pm2_5201512100','pm2_5201512101','pm2_5201512102'.....]
-    '''
-    s_ = start_time.split('-')
-    start = int(time.strftime('%j', time.strptime(start_time, '%y-%m-%d')))
-    end = int(time.strftime('%j', time.strptime(end_time, '%y-%m-%d')))
-    all_files = []
-    for ii in range(start, end + 1):
-        tmp_time = time.strptime('15-%s' % ii, '%y-%j')
-        tmp_day = time.strftime('%Y%m/%d/', tmp_time)
-        tmp_day2 = time.strftime('pm2_5%Y%m%d', tmp_time)
-        b = [tmp_day + tmp_day2 ]
-        all_files += b
-    return all_files;
+
